520b59f8a762411f756a31ee119396bf7df58971
freemind <guaripolo007@gmail.com>
2022-11-03 15:23:55 -0400

    finalizacion clase 2 con todos los metodos CRUD, estandarizacion de response

---
+++ src/expressExample/routes/response.js
@@ -0,0 +1,14 @@
+//estandarizacion de requests
+/**
+ *
+ * @param {Object} args
+ * @param {Boolean} args.error
+ * @param {Object} args.message
+ * @param {Number} args.status
+ * @param {import('express')} args.res
+ */
+const response = ({ error = true, message, status = 500, res }) => {
+  res.status(status).send({ error, message })
+}
+
+module.exports = response
--- src/expressExample/routes/user.js
+++ src/expressExample/routes/user.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const { nanoid } = require('nanoid')
 // conjunto de rutas para la entidad user
 const users = require('../data/user')
+const response = require('./response')
 // CRUD
 /**
  * CREATE - POST
@@ -14,11 +15,8 @@ const UserRouter = Router()
 
 UserRouter.route('/user')
 .get((req, res) => {
-    res.send({
-        message: users,
-        error: false
+    response({ error: false, message: users, res, status: 200 })
     })
-})
 .post((req, res) => {
     console.log('reqqqq', req.body)
     const { body: { name, email } } = req 
@@ -27,11 +25,42 @@ UserRouter.route('/user')
         name,
         email
     })
-    res.status(201).send({
-        message: users,
-        error: false
+    response({ error: false, message: users, res, status: 201 })
+
+})
+UserRouter.route('/user/:id')
+.delete((req,res) => {
+    const { params: { id }} = req
+    const userIndex = users.findIndex(user => user.id === id)
+    if(userIndex === -1) 
+        return response({
+            message: `User with id: ${id} was not found`,
+            res,
+            status: 404
+    })
+    users.splice(userIndex, 1)
+    response({ error: false, message: users, res, status: 200 })
+})
+.patch((req, res) => {
+    const {
+    body: { name, email },
+    params: { id }
+    } = req
+    const userIndex = users.findIndex(user => user.id === id)
+
+    if (userIndex === -1)
+    return response({
+        message: `User with id: ${id} was not found`,
+        res,
+        status: 404
     })
 
+    users.splice(userIndex, 1, {
+    ...users[userIndex],
+    ...(name && { name }),
+    ...(email && { email })
+    })
+    response({ error: false, message: users, res, status: 200 })
 })
 
 module.exports = UserRouter
